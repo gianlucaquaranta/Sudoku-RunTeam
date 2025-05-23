810c90e928c70e6bf55dd9fac8f3d1ff03ab17e3
Gianluca Quaranta <gianluca.q.rm@gmail.com>
2025-05-23 18:45:59 +0200

    First commit

---
+++ app/src/main/java/it/uniroma2/RunTeam/Sudoku/MainActivity.kt
@@ -0,0 +1,47 @@
+package it.uniroma2.RunTeam.Sudoku
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.uniroma2.RunTeam.Sudoku.ui.theme.SudokuRunteamTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SudokuRunteamTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SudokuRunteamTheme {
+        Greeting("Android")
+    }
+}
