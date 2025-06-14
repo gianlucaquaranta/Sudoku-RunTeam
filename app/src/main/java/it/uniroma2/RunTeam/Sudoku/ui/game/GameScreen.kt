85701604ab44067a370f802d12ed223ffe26303b
SMITH <smithvalenzuela324@gmail,com>
2025-06-14 13:50:07 +0200

    Commit Smith adattamento della griglia e number pad allo schermo girato

--- app/src/main/java/it/uniroma2/RunTeam/Sudoku/ui/game/GameScreen.kt
+++ app/src/main/java/it/uniroma2/RunTeam/Sudoku/ui/game/GameScreen.kt
@@ -1,37 +1,22 @@
 package it.uniroma2.RunTeam.Sudoku.ui.game
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import it.uniroma2.RunTeam.Sudoku.model.Difficulty
 import it.uniroma2.RunTeam.Sudoku.ui.game.ViewModel.GameViewModel
 import it.uniroma2.RunTeam.Sudoku.ui.game.components.GameTopBar
-import it.uniroma2.RunTeam.Sudoku.ui.game.components.NumberPad
-import it.uniroma2.RunTeam.Sudoku.ui.game.components.SudokuGrid
-import it.uniroma2.RunTeam.Sudoku.ui.game.components.SuggestionButton
 import it.uniroma2.RunTeam.Sudoku.R
 import androidx.navigation.NavHostController
 import it.uniroma2.RunTeam.Sudoku.CelebrationConfetti
+import it.uniroma2.RunTeam.Sudoku.ui.game.components.ResponsiveGameLayout
 
 @Composable
 fun GameScreen(navController: NavHostController,gameViewModel: GameViewModel = viewModel()) {
